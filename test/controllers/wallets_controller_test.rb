e8fe3878bca527d7709a8e41d7c75820b7db37d4
moushumiseal <moushumiseal@gmail.com>
2025-05-09 19:10:34 +0800

    Fixing failing test in Wallet controller

--- test/controllers/wallets_controller_test.rb
+++ test/controllers/wallets_controller_test.rb
@@ -5,36 +5,35 @@ class WalletsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:alice)
     @wallet = @user.wallet
-
-    # User Sign in by stubbing Devise
     sign_in @user
   end
 
   test "should deposit money" do
-    post deposit_wallet_url(@wallet), params: { amount_cents: 100_00, currency: "SGD" }
+    # Make sure the amount is greater than 0
+    post deposit_wallet_url(@wallet), params: { amount: 100.0 }
     assert_response :success
-    assert_equal 100_00, @wallet.reload.balance_cents
+    assert_equal 100.0, @wallet.reload.balance.to_f
   end
 
   test "should withdraw money if sufficient balance" do
-    @wallet.update!(balance_cents: 200_00)
-    post withdraw_wallet_url(@wallet), params: { amount_cents: 100_00, currency: "SGD" }
+    @wallet.update!(balance: 200.0)
+    post withdraw_wallet_url(@wallet), params: { amount: 100.0 }
     assert_response :success
-    assert_equal 100_00, @wallet.reload.balance_cents
+    assert_equal 100.0, @wallet.reload.balance.to_f
   end
 
   test "should not withdraw money if insufficient balance" do
-    post withdraw_wallet_url(@wallet), params: { amount_cents: 200_00, currency: "SGD" }
+    post withdraw_wallet_url(@wallet), params: { amount: 200.0 }
     assert_response :unprocessable_entity
   end
 
   test "should transfer money" do
     bob = users(:bob)
-    @wallet.update!(balance_cents: 300_00)
+    @wallet.update!(balance: 300.0)
 
-    post transfer_wallet_url(@wallet), params: { receiver_id: bob.id, amount_cents: 150_00, currency: "SGD" }
+    post transfer_wallet_url(@wallet), params: { receiver_id: bob.id, amount: 150.0 }
     assert_response :success
-    assert_equal 150_00, @wallet.reload.balance_cents
-    assert_equal 150_00, bob.wallet.reload.balance_cents
+    assert_equal 150.0, @wallet.reload.balance.to_f
+    assert_equal 150.0, bob.wallet.reload.balance.to_f
   end
 end
